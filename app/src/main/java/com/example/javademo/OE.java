ca868a6e99f6e8bcc4ec96c7b7b8f90426d05c9e
MPB <mp4learning@gmail.com>
2021-11-05 20:33:10 +0530

    Threas synchronization with Odd Even

---
+++ app/src/main/java/com/example/javademo/OE.java
@@ -0,0 +1,77 @@
+package com.example.javademo;
+
+import android.util.Log;
+
+public class OE {
+
+    private final String TAG = OE.class.getSimpleName();
+    private Object object = new Object();
+    private int value = 1;
+
+    public OE() {
+       oddThread.start();
+       evenThread.start();
+    }
+
+    Thread oddThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            printOdd();
+        }
+    });
+
+    Thread evenThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            printEven();
+        }
+    });
+
+    private void printOdd() {
+
+        while(true) {
+            synchronized (object) {
+                while (value%2 == 0) {
+                    try {
+                        object.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Log.v(TAG,"Odd Thread:"+value);
+                value++;
+                object.notify();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    private void printEven() {
+        while (true) {
+            synchronized (object) {
+                while (value%2 !=0) {
+                    try {
+                        object.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Log.v(TAG,"Even Thread:"+value);
+                value++;
+                object.notify();
+                try{
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
