107afbf5c82a6908d4a0bf69b8b93d6ce5fc4d6e
MPB <mp4learning@gmail.com>
2021-11-05 21:39:20 +0530

    Thread Deadlock.

---
+++ app/src/main/java/com/example/javademo/Deadlock.java
@@ -0,0 +1,62 @@
+package com.example.javademo;
+
+import android.util.Log;
+
+public class Deadlock {
+
+    private final String TAG = Deadlock.class.getSimpleName();
+    private Object object = new Object();
+    private Object object1 = new Object();
+    Thread thread1 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            function1();
+
+        }
+    });
+    Thread thread2 = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            function2();
+
+        }
+    });
+    public Deadlock() {
+        thread1.start();
+        thread2.start();
+    }
+
+    private void function1() {
+        synchronized (object1) {
+            Log.v(TAG, "Thread 1 object :");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            synchronized (object) {
+                Log.v(TAG, "Thread 1 object1 :");
+            }
+        }
+    }
+
+    private void function2() {
+        synchronized (object) {
+            Log.v(TAG, "Thread 2 object :");
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            synchronized (object1) {
+                Log.v(TAG, "Thread 2 object1 :");
+            }
+        }
+
+    }
+
+
+}
